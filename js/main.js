66c2412e9894f6efe6eaf0cc555b1ea83fa05b31
ANGELICA ALONSO ORTIZ <130294701+ANGELICA-DATA-SCIENCE@users.noreply.github.com>
2024-09-05 20:02:54 -0600

    Corrección del código JavaScript para su funcionalidad

--- js/main.js
+++ js/main.js
@@ -28,25 +28,31 @@ function createCards(products) {
             <p class="card-text">${p.brand}</p>
             <p class="card-text">${p.description}</p>
             <p class="card-text">${p.rating}</p>
-            <a href="" class="btn btn-primary">Más informacion...</a>
+            <button type="button" style="align-self: flex-end;" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal_${p.id}">
+            Más información...
+            </button>
+        </div>
+        </div>
+
             <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal fade" id="exampleModal_${p.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">Más información...</button>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">${p.name}</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
 
-
-
-
-            
+                <div class="modal-body">
+                ${p.description}
+                <p class ="text-end">$ ${p.price} USD </p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                </div>
         </div>
         </div>
-
-
-        `);
+`);
     });
 }//creatCards
 
